24bce5371e07330e95b0ff80afb6b45718282fcf
williiamdr <williiamvlog@gmail.com>
2024-03-04 20:08:17 -0800

    almost

--- Controllers/places.js
+++ Controllers/places.js
@@ -1,7 +1,15 @@
 const router = require('express').Router()
+const express = require('express')
+const app = express()
 
 router.get('/', (req, res) => {
-    res.send('Test')
+    res.render('./places/index')
 })
+
+// GET /places
+app.get('/', (req, res) => {
+    res.render('places/index')
+  })
+  
 
 module.exports = router
---
+++ views/places/index.jsx
@@ -0,0 +1,45 @@
+const React = require('react')
+const Def = require('./index')
+
+// GET /places
+app.get('/', (req, res) => {
+    let places = [{
+        name: 'H-Thai-ML',
+        city: 'Seattle',
+        state: 'WA',
+        cuisines: 'Thai, Pan-Asian',
+        pic: 'http://placekitten.com/250/250'
+      }, {
+        name: 'Coding Cat Cafe',
+        city: 'Phoenix',
+        state: 'AZ',
+        cuisines: 'Coffee, Bakery',
+        pic: 'http://placekitten.com/250/250'
+      }]
+      
+      res.render('places/index', { places })
+  })
+  
+  function index (data) {
+    let placesFormatted = data.places.map((place) => {
+      return (
+        <div>
+          <h2>{place.name}</h2>
+          <img src={place.pic} alt={place.name}/>
+        </div>
+      )
+    })
+    return (
+      <Def>
+          <main>
+              <h1>PLACES INDEX PAGE</h1>
+              {placesFormatted}
+          </main>
+      </Def>
+  )
+  }
+  
+  
+  
+
+module.exports = index
