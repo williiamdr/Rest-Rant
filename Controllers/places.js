7fb536c6441cc7006041aee1d785c257d26f7477
williiamdr <williiamvlog@gmail.com>
2024-03-06 17:58:08 -0800

    added forms to add new restaurants

--- Controllers/places.js
+++ Controllers/places.js
@@ -1,21 +1,27 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
+router.get('/new', (req, res) => {
+    res.render('places/new')
+  })
+
+  router.post('/', (req, res) => {
+    console.log(req.body)
+    if (!req.body.pic) {
+      // Default image if one is not provided
+      req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+      req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+      req.body.state = 'USA'
+    }
+    res.send('POST /places')
+  })
+  
+  
 
 router.get('/', (req, res) => {
-    let places = [
-        {
-            name: 'H-Thai-ML',
-            city: 'Seattle',
-            state: 'WA',
-            cuisines: 'Thai, Pan-Asian',
-            pic: '/images/soup.jpg'
-        }, {
-            name: 'Coding Cat Cafe',
-            city: 'Phoenix',
-            state: 'AZ',
-            cuisines: 'Coffee, Bakery',
-            pic: '/images/toast.jpg'
-        }
-    ]
     res.render('places/index', { places })
 })
 
--- views/home.jsx
+++ views/home.jsx
@@ -11,7 +11,7 @@ function home () {
               Photo by <a href="https://unsplash.com/@ellaolsson?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Ella Olsson</a> on <a href="https://unsplash.com/photos/assorted-fruits-and-vegetable-on-brown-wooden-chopping-board-2IxTgsgFi-s?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a> 
               </div>
               <a href="/places">
-  <button className="btn-primary">Places to rant or Rave About</button>
+  <button className="btn-primary">Places Page</button>
 </a>
           </main>
       </Def>
